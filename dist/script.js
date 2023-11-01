1f9693f32cc93469b02659e64d3478f40cb899ab
sidxh <totallysiddhant@gmail.com>
2023-11-02 00:16:27 +0530

    migrated from js to ts

---
+++ dist/script.js
@@ -0,0 +1,19 @@
+"use strict";
+var characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", ",", "|", ":", ";", "<", ">", ".", "?",
+    "/"];
+var genButEl = document.getElementById("genBut-el");
+var passDiv1El = document.getElementById("passDiv1");
+var passDiv2El = document.getElementById("passDiv2");
+function resetPassword() {
+    passDiv1El.textContent = "";
+    passDiv2El.textContent = "";
+}
+function generatePassword() {
+    resetPassword();
+    for (var i = 0; i < 15; i++) {
+        var password1 = Math.floor(Math.random() * characters.length);
+        var password2 = Math.floor(Math.random() * characters.length);
+        passDiv1El.textContent += characters[password1];
+        passDiv2El.textContent += characters[password2];
+    }
+}
